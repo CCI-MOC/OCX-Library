866866f2432633d4ef444cd7d19733a5d5723513
Lucas H. Xu <xuh@wireless1x-155-41-98-36.bu.edu>
2015-06-01 14:45:21 -0400

    added glance and keystone
    Need to discuss what we should do for cinder and might also token?

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
+#libs
 from keystoneclient.auth.identity import v2
-import novaclient.v1_1.client as nvclient
+import novaclient.v2.client as nvclient
 import glanceclient.v2.client as glclient
 import keystoneclient.v2_0.client as ksclient
 
@@ -7,9 +8,9 @@ from keystoneclient import session
 from novaclient import client
 
 
-session_list = []
-def init_sessions(tenant_name, username, password, auth_urls):
-	
+#Session
+def init_sessions(username, password,tenant_name,auth_urls):
+	session_list =[]
 	for url in auth_urls:
 
 		auth = v2.Password(auth_url=url,
@@ -21,13 +22,47 @@ def init_sessions(tenant_name, username, password, auth_urls):
 		session_list.append(sess)
 
 	return session_list
+#keystone
+def init_keystone_clients(username, password,tenant_name,auth_urls):
+	keystone_client_list=[]
+	for url in auth_urls:
+ 		keystone = ksclient.Client(auth_url = url, username = username,
+ 			password = password, tenant_name = tenant_name)
+ 		keytone_client_list.append(keystone)
 
+	return keystone_client_list
 
+#glance
+def init_glance_clients(keystone_client_list):
+	glance_client_list = []
+	for keystone in keystone_client_list:
+		glance_endpoint = keystone.service_catalog.url_for(service_type='image')
+		glance = glclient.Client(glance_endpoint, token = keystone.auth_token)
+		glance_client_list.append(glance)
+
+	return glance_client_list
+
+#nova
 def init_nova_clients(version, session_list):
 	nova_client_list = []
 	for session in session_list:
-		nova = client.Client(version, session)
+		nova = client.Client(version, session=session_list)
 		nova_client_list.append(nova)
 
 	return nova_client_list
 
+## Need to Discuss
+#nova
+def init_cinder_clients():
+
+	return cinder_client_list	
+#token
+# def init_tokens(username, password,tenant_name,auth_urls):
+# 	token_list = []
+# 	for url in auth_urls:
+# 		keystone = ksclient.Client(auth_url = url, username = username,
+# 			password = password, tenant_name = tenant_name)
+# 		token = keystone.auth_tokens
+# 		token_list.append(token)
+	
+# 	return token_list
--- test.py
+++ test.py
@@ -3,22 +3,15 @@ import api
 import socks
 import socket
 
-enpoint_list = ['http://140.247.152.200:5000/v2.0']
+url_list = ['http://140.247.152.207:5000/v2.0']
 
-tenant_name ='admin'
+tenant_name ='moc-ops-experiments'
 username = 'xuh'
 password = 'xuhang0507'
 
 
-# Set up SOCKS proxy usage:
-s = socks.socksocket()
+session_list = api.init_sessions(tenant_name, username, password, url_list)
 
-# Set up the Port number as the one used for connecting Harvard Cluster
-socks.set_default_proxy(socks.SOCKS5, 'localhost', 5507)
-socket.socket = socks.socksocket
-
-
-session_list = api.init_sessions(tenant_name, username, password, enpoint_list)
-nova_client_list  = api.init_nova_clients('1.1', session_list)
+nova_client_list  = api.init_nova_clients('2', session_list)
 for nova in nova_client_list:
-	print nova.images.list()
+	print nova.flavors.list()
