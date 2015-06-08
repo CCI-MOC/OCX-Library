31dece1ae4a923eb12f480c34663b4e281c966a9
Lucas H. Xu <xuh@wireless1x-155-41-96-154.bu.edu>
2015-06-08 12:07:02 -0400

    Setting up Pytest

--- tests/api_test_with_two_endpoints.py
+++ tests/api_test_with_two_endpoints.py
@@ -1,5 +1,25 @@
-import api
-# Socks
+# Copyright 2013-2014 Massachusetts Open Cloud Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS
+# IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.  See the License for the specific language
+# governing permissions and limitations under the License.
+
+"""Unit tests for api.py"""
+
+from ocx-library import api
+import pytest
+
+
+
+
 url_list = ['http://140.247.152.150:5000/v2.0', 'http://140.247.152.35:5000/v2.0' , 'http://140.247.152.189:5000/v2.0']
 
 tenant_name ='admin'
@@ -7,35 +27,35 @@ username = 'admin'
 password = 'ocx'
 
 
-session_list = phaseOneOCX.init_sessions(username, password,tenant_name,url_list)
+session_list = api.init_sessions(username, password,tenant_name,url_list)
 
 #nova
-nova_client_list  = phaseOneOCX.init_nova_clients('2', session_list)
-nova_service_object_list = phaseOneOCX.nova_service_version_2(nova_client_list,"flavors")
+nova_client_list  = api.init_nova_clients('2', session_list)
+nova_service_object_list = api.nova_service_version_2(nova_client_list,"flavors")
 for service in nova_service_object_list:
 	print service.list()
 
 #keystone
-keystone_client_list = phaseOneOCX.init_keystone_clients(username, password,tenant_name,url_list)
-keystone_service_object_list = phaseOneOCX.keystone_service(keystone_client_list,"users")
+keystone_client_list = api.init_keystone_clients(username, password,tenant_name,url_list)
+keystone_service_object_list = api.keystone_service(keystone_client_list,"users")
 for keystone in keystone_service_object_list:
 	for member in keystone.list():
 		print member.name
 
 #glance
-glance_client_list = phaseOneOCX.init_glance_clients(keystone_client_list)
-glance_service_object_list = phaseOneOCX.glance_service(glance_client_list,"images")
+glance_client_list = api.init_glance_clients(keystone_client_list)
+glance_service_object_list = api.glance_service(glance_client_list,"images")
 for service in glance_service_object_list:
 	print list(service.list())[0]['name']
 
 #cinder
-cinder_client_list = phaseOneOCX.init_cinder_clients(keystone_client_list)
-cinder_service_object_list = phaseOneOCX.cinder_service(cinder_client_list,"volumes")
+cinder_client_list = api.init_cinder_clients(keystone_client_list)
+cinder_service_object_list = api.cinder_service(cinder_client_list,"volumes")
 for service in cinder_service_object_list:
 	print service.list()
 	
 #swift
-swift_client_list = phaseOneOCX.init_swift_clients(keystone_client_list)
-swift_service_object_list = phaseOneOCX.swift_service(swift_client_list,"head_account")
+swift_client_list = api.init_swift_clients(keystone_client_list)
+swift_service_object_list = api.swift_service(swift_client_list,"head_account")
 for service in swift_service_object_list:
 	print service
