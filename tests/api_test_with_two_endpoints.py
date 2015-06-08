041f5dc525889153bd63e149c22fcc3fe9ef278e
Lucas H. Xu <xuh@wireless1x-155-41-96-154.bu.edu>
2015-06-08 11:13:51 -0400

    Upload latest Test python script for API

--- tests/api_test_with_two_endpoints.py
+++ tests/api_test_with_two_endpoints.py
@@ -1,4 +1,4 @@
-import phaseOneOCX
+import api
 # Socks
 url_list = ['http://140.247.152.150:5000/v2.0', 'http://140.247.152.35:5000/v2.0' , 'http://140.247.152.189:5000/v2.0']
 
@@ -9,15 +9,33 @@ password = 'ocx'
 
 session_list = phaseOneOCX.init_sessions(username, password,tenant_name,url_list)
 
+#nova
 nova_client_list  = phaseOneOCX.init_nova_clients('2', session_list)
-#for nova in nova_client_list:
-#	print nova.flavors.list()
-#phaseOneOCX.nova_service(nova_client_list,"flavors")
-#print "now printing image list"
-#phaseOneOCX.nova_service(nova_client_list,"images")
-
-print "working with version 2 of nova service"
-service_object_list = phaseOneOCX.nova_service_version_2(nova_client_list,"flavors")
-for service in service_object_list:
+nova_service_object_list = phaseOneOCX.nova_service_version_2(nova_client_list,"flavors")
+for service in nova_service_object_list:
 	print service.list()
 
+#keystone
+keystone_client_list = phaseOneOCX.init_keystone_clients(username, password,tenant_name,url_list)
+keystone_service_object_list = phaseOneOCX.keystone_service(keystone_client_list,"users")
+for keystone in keystone_service_object_list:
+	for member in keystone.list():
+		print member.name
+
+#glance
+glance_client_list = phaseOneOCX.init_glance_clients(keystone_client_list)
+glance_service_object_list = phaseOneOCX.glance_service(glance_client_list,"images")
+for service in glance_service_object_list:
+	print list(service.list())[0]['name']
+
+#cinder
+cinder_client_list = phaseOneOCX.init_cinder_clients(keystone_client_list)
+cinder_service_object_list = phaseOneOCX.cinder_service(cinder_client_list,"volumes")
+for service in cinder_service_object_list:
+	print service.list()
+	
+#swift
+swift_client_list = phaseOneOCX.init_swift_clients(keystone_client_list)
+swift_service_object_list = phaseOneOCX.swift_service(swift_client_list,"head_account")
+for service in swift_service_object_list:
+	print service
